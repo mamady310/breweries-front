8b9f642674e2a9f7bcbf6e82f996c560cfdffb6f
Mamady Sillah <supurpisces310@msn.com>
2020-01-03 09:50:06 -0500

    added city home home nav.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
+// import React from "react";
 import './App.css';
+import Home from "./Home";
+import Name from "./Name";
+import City from "./City";
+import Nav from "./Nav";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-function App() {
+class App extends Component {
+  constructor (props){
+    super (props)
+  this.state = {
+    breweryData: [],
+ }
+ };
+
+ async componentDidMount() {
+  fetch('http://localhost:5000/')
+  .then(res => res.json())
+  .then(res => {
+    this.setState({breweryData: res})
+    console.log(this.state.breweryData)
+  })
+  }
+render () {
+  let list = this.state.breweryData.map(item => {
+  return <Name key={item.name} name={item.name}/>
+  })
   return (
-    <div className="App">
-      
-    </div>
+ <Router>
+   <div className="App">
+     <Nav />
+     <Switch>
+       <Route path ="/" exact component={Home}/>
+       <Route path="/Name" component={Name}><ul>{list}</ul></Route>
+       <Route path="/City" component={City}/>
+     </Switch>
+  <ul>{list}</ul>
+   </div>
+ </Router>
   );
+}
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Nav extends React.Component {
+    render() {
+       return (
+          <nav>
+            <ul className="nav-links">
+                <Link to="/">
+                <li>Home</li>
+                </Link>
+                <Link to="/Name">
+                <li>Brewery by Name</li>
+                </Link>
+                <Link to="/City">
+                <li>Brewery by City</li>
+                </Link>
+            </ul>
+
+          </nav>
+       )
+    }
+ }
+ export default Nav;
