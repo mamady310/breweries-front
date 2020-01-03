7d244b54d3d5432f1422f697e2248fac117c458d
Mamady Sillah <supurpisces310@msn.com>
2020-01-03 11:01:39 -0500

    added fetch and redner function to name.js component in hopes that the list would only render on that component

--- src/Name.js
+++ src/Name.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 class Name extends React.Component {
+    constructor (props){
+        super (props)
+      this.state = {
+        breweryData: [],
+     }
+     };
+    
+     async componentDidMount() {
+      fetch('http://localhost:5000/')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({breweryData: res})
+        console.log(this.state.breweryData)
+      })
+      }
     render() {
+        let list = this.state.breweryData.map(item => {
+            return <Name key={item.name} name={item.name}/>
+            })  
        return (
           <div>
-             <ul>{this.props.name}</ul>
+             <ul>{list}</ul>
           </div>
        )
     }
